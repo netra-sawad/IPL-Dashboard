481347d743a385fb443fea5ab9173ae62e5f745b
Nerta <netra@mbank.com.np>
2021-06-11 00:12:57 +0545

    Completed UI Component For Team Page

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,10 +1,14 @@
 import { React } from 'react'
-
-export const MatchSmallCard = ({match}) => {
+import { Link } from 'react-router-dom'
+export const MatchSmallCard = ({teamName,match}) => {
+  if(!match) return null;
+  const otherTeam = match.team1 === teamName ?match.team2 : match.team1;
+  const otherTeamRoute = `/teams/${otherTeam}`;
     return(
         <div className="MatchSmallCard">
-          <p>{match.team1} vs {match.team2}</p>
+          <h3> vs  <Link to={otherTeamRoute}>{otherTeam }</Link></h3>
           {/* <p>{matches.team1} vs {matches.team2}</p> */}
+          <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
         </div>
     );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,4 +1,5 @@
 import  {React, useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
@@ -7,25 +8,29 @@ export const TeamPage = () => {
     // const [team, setTeam] = useState({});
     const[team,setTeam] = useState({matches :[]});
     // Here team is a state , setTeam is a way to populate it
+    const{teamName} = useParams();
     
     // Here, When the component loads with the empty array
     useEffect(
         () => {
             const fetchMatches = async () =>{
-                const response = await fetch('http://localhost:8080/team/Chennai%20Super%20Kings');
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 console.log(data);
                 setTeam(data);
             };
             fetchMatches();
 
-        }, []
+        }, [teamName]
     );
+    if(!team || !team.teamName){
+        return <h1> Team not found</h1>
+    }
     return(
         <div className = "TeamPage">
             <h1> {team.teamName} </h1>
-            <MatchDetailCard match={team.matches[0]}/>
-            {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
+            <MatchDetailCard teamName= {team.teamName} match={team.matches[0]}/>
+            {team.matches.slice(1).map(match => <MatchSmallCard teamName= {team.teamName} match={match} />)}
             {/* {team.matches && team.matches.map(match =>(<MatchSmallCard match = {match} />))} */}
                  </div>
     );
