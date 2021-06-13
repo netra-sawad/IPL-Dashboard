8b38574e33748b141b01b3809b05500405afe180
Nerta <netra@mbank.com.np>
2021-06-14 01:06:12 +0545

    Add backend match Api

--- src/main/java/com/fwu/ns/ipl_dashboard/controller/TeamController.java
+++ src/main/java/com/fwu/ns/ipl_dashboard/controller/TeamController.java
@@ -1,15 +1,19 @@
 package com.fwu.ns.ipl_dashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fwu.ns.ipl_dashboard.Model.Match;
 import com.fwu.ns.ipl_dashboard.Model.Team;
 import com.fwu.ns.ipl_dashboard.repository.MatchRepository;
 import com.fwu.ns.ipl_dashboard.repository.TeamRepository;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @CrossOrigin
@@ -34,6 +38,18 @@ public class TeamController {
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName,4));
         return team;
         
+    }
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year)
+    {
+       LocalDate startDate = LocalDate.of(year, 1, 1);
+       LocalDate endDate = LocalDate.of(year+1, 1, 1);
+       return this.matchRepository.getMatchByTeamBetweenDates(teamName, startDate, endDate);
+    //    return this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc( teamName, startDate, endDate,
+    //        teamName,
+    //        startDate,
+    //        endDate
+    //    );
 
     }
     
